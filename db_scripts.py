cde7a8d54a72a756130c5523e5c7291edb60c733
Robert Godlewski <robtechg5@gmail.com>
2025-02-28 16:09:56 -0800

    added in a draft to upload csv to db

--- db_scripts.py
+++ db_scripts.py
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS election_year (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     year INTEGER UNIQUE,
     total_population INTEGER NOT NULL,
-    total_electoral INTEGER
+    total_electoral INTEGER NOT NULL
 );
 """
 
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS office_position (
 candidates_script = """
 CREATE TABLE IF NOT EXISTS candidates (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
 );
 """
 
@@ -46,5 +46,13 @@ CREATE TABLE IF NOT EXISTS candidates_to_party (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     candidate_id REFERENCES candidates (id) ON DELETE CASCADE,
     party_id REFERENCES political_party (id) ON DELETE CASCADE
+);
+"""
+
+candidates_to_office_script = """
+CREATE TABLE IF NOT EXISTS candidates_to_office (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    candidate_id REFERENCES candidates (id) ON DELETE CASCADE,
+    office_id REFERENCES office_position (id) ON DELETE CASCADE
 );
 """
--- upload_csv.py
+++ upload_csv.py
@@ -50,38 +50,84 @@ def _strToInt(strnum: str) -> int:
         multi += 1000
     return num
 
-# Fix this
-def _handleData(data):
-    # We can just calculate data[6] and data[9] later
-    election_year_data = {
-        'year': data[0],
-        'total_population': _strToInt(data[5]),
-        'total_electoral': data[8],
+def _createOrGetOfficePosition(position: str) -> dict:
+    cur.execute("INSERT OR IGNORE INTO office_position (position) VALUE ( ? )", (position,))
+    cur.execute("SELECT * FROM office_position WHERE position = ? ", (position,))
+    raw = cur.fetchone()
+    return {
+        'id': raw[0],
+        'position': raw[1]
     }
-    political_party_data = {
+
+def getOfficeData() -> list:
+    president = _createOrGetOfficePosition('President')
+    vice_president = _createOrGetOfficePosition('Vice President')
+    return [president, vice_president]
+
+def createOrGetElectionYear(year: int, total_population: int, total_electoral: int) -> dict:
+    cur.execute("INSERT OR IGNORE INTO election_year (year, total_population, total_electoral) VALUES ( ?, ?, ? )", (year, total_population,total_electoral,))
+    cur.execute("SELECT * FROM election_year WHERE year = ? ", (year,))
+    data = cur.fetchone()
+    return {
+        'id': data[0],
+        'total_population': data[1],
+        'total_electoral': data[2]
+    }
+
+def createOrGetParty(party: str) -> dict:
+    cur.execute("INSERT OR IGNORE INTO political_party (party) VALUES ( ? )", (party,))
+    cur.execute("SELECT * FROM political_party WHERE party = ? ", (party,))
+    data = cur.fetchone()
+    return {
+        'id': data[0],
         'party': data[1]
     }
-    office_positions_data = [
-        {'position': 'President'},
-        {'position': 'Vice President'},
-    ]
-    candidates_data = [
-        # President
-        {'name': data[2]},
-        # Vice President
-        {'name': data[3]}
-    ]
-    candidates_to_elections_data = {
-        'popular_vote': _strToInt(data[4]),
-        'electoral_vote': data[7],
-        'notes': data[10],
-        'won': data[11],
+
+def createOrGetCandidate(candidate: str) -> dict:
+    cur.execute("INSERT OR IGNORE INTO cadidates (name) VALUES ( ? )", (candidate,))
+    cur.execute("SELECT * FROM cadidates WHERE name = ? ", (candidate,))
+    data = cur.fetchone()
+    return {
+        'id': data[0],
+        'name': data[1]
     }
-    # Add in sql and more data later
-    candidates_to_party_data = {}
+
+def createCandidateElection(popular_vote: int, electoral_vote: int, notes: str, won: bool, election_id: int, candidate_id: int) -> None:
+    cur.execute("INSERT OR IGNORE INTO candidates_to_elections (popular_vote, electoral_vote, notes, won, election_id, candidate_id) VALUES ( ?, ?, ?, ?, ?, ? )", (popular_vote, electoral_vote, notes, won, election_id, candidate_id,))
+
+def createCandidateParty(candidate_id: int, party_id: int) -> None:
+    cur.execute("INSERT OR IGNORE INTO candidates_to_party (candidate_id, party_id) VALUES ( ?, ? )", (candidate_id, party_id,))
+
+def createCandidateOffice(candidate_id: int, office_id: int) -> None:
+    cur.execute("INSERT OR IGNORE INTO candidates_to_office (candidate_id, office_id) VALUES ( ?, ? )", (candidate_id, office_id,))
+
+def handleData(data: list) -> None:
+    office_data = getOfficeData()
+    # We can just calculate data[6] and data[9] later
+    election_year = data[0]
+    party = data[1]
+    president_name = data[2]
+    vice_president_name = data[3]
+    popular_vote = _strToInt(data[4])
+    total_population = _strToInt(data[5])
+    electoral_vote = data[7]
+    total_electoral = data[8]
+    notes = data[10]
+    won = data[11]
+    year_data = createOrGetElectionYear(election_year, total_population, total_electoral)
+    party_data = createOrGetParty(party)
+    president_data = createOrGetCandidate(president_name)
+    createCandidateElection(popular_vote, electoral_vote, notes, won, election_id=year_data['id'], candidate_id=president_data['id'])
+    createCandidateParty(candidate_id=president_data['id'], party_id=party_data['id'])
+    createCandidateOffice(candidate_id=president_data['id'], office_id=office_data[0]['id'])
+    vice_president_data = createOrGetCandidate(vice_president_name)
+    createCandidateElection(popular_vote, electoral_vote, notes, won, election_id=year_data['id'], candidate_id=vice_president_data['id'])
+    createCandidateParty(candidate_id=vice_president_data['id'], party_id=party_data['id'])
+    createCandidateOffice(candidate_id=vice_president_data['id'], office_id=office_data[1]['id'])
+
 
 try:
-    # We will not need the first 2 lines within the csv file
+    # We will not need the first couple of lines within the csv file
     header = 1
     raw_data = pandas.read_csv(csv_file,header=header).values
     print(raw_data.shape)
@@ -91,7 +137,8 @@ try:
     num = _strToInt(raw_data[0][4])
     print(f'{num}: {type(num)}')
     # numpy.save(npy_temp_name,data)
-    # Need to use the _handleData function here
+    for row in raw_data:
+        handleData(row)
     print('Saved new data.')
 except:
     print('There is no data to mine with the necessary information!')
